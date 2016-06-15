ce2034cc4108fcff4b57b9496c45eaeea833f994
Kevin Fowle <email": null>
2016-06-15 16:42:07 -0500

    fix merge issues

--- killer-tomatoes/config/routes.rb
+++ killer-tomatoes/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   resources :user
   resources :sessions, only: [:new, :create, :destroy]
 
-  root 'movies#index'
 
   resources :movies, only: [:show]
+  root 'movies#index'
 
 end
