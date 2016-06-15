e6cfbfbbe731d2d20b5f766e7c453660289cc7bc
Kevin Fowle <ksfowle@comcast.net>
2016-06-15 15:33:52 -0500

    Update 20160615193217_create_users.rb

--- killer-tomatoes/db/migrate/20160615193217_create_users.rb
+++ killer-tomatoes/db/migrate/20160615193217_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :name
+      t.string :email
       t.string :password_digest
 
       t.timestamps null: false
