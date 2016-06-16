90fd191f324c91959b248a28ef2e2e40cf381910
Gabriel Zurita & Tim Lantz <pair+gzurita.tlantz77@devbootcamp.com>
2016-06-16 09:44:00 -0500

    basic profile page

--- killer-tomatoes/app/controllers/users_controller.rb
+++ killer-tomatoes/app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
--- killer-tomatoes/app/models/user.rb
+++ killer-tomatoes/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :reviews, foreign_key: 'reviewer_id'
+  has_many :reviewed_movies, through: :reviews, source: :movies
   has_many :comments, foreign_key: "commenter_id"
   has_many :ratings, foreign_key: "rater_id"
   has_secure_password
