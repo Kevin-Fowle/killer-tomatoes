3847a1e7e7ff93cbd2f032bc8d84d6f0efc6dcf6
Gabriel Zurita & Tim Lantz <pair+gzurita.tlantz77@devbootcamp.com>
2016-06-16 19:17:13 -0500

    methods for calculating average movie scores

--- killer-tomatoes/app/models/movie.rb
+++ killer-tomatoes/app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
+  has_many :reviewers, through: :reviews
+
+  def overall_score
+    self.reviews.average(:score).round(1)
+  end
+
+  def gold_score
+    gold_reviews = self.reviews.select {|review|
+        review.reviewer.status == "Gold" }
+    gold_reviews.average(:score).round(1)
+  end
 end
--- killer-tomatoes/app/models/user.rb
+++ killer-tomatoes/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   has_many :reviews, foreign_key: 'reviewer_id'
-  has_many :reviewed_movies, through: :reviews, source: :movies
+  has_many :reviewed_movies, through: :reviews, source: :movie
   has_many :comments, foreign_key: "commenter_id"
   has_many :ratings, foreign_key: "rater_id"
   has_secure_password
